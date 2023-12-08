1970232ce0af842786fc6ea22741f40370117831
Hadi90fe <elhadiii90@gmail.com>
2023-12-08 14:34:13 -0500

    :sparkles: feat: enhanced Navbar component

--- app/ui/dashboard/navbar/navbar.jsx
+++ app/ui/dashboard/navbar/navbar.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+"use client";
+
+import { usePathname } from "next/navigation";
+import styles from "./navbar.module.css";
+import { MdSearch } from "react-icons/md";
+import FilterIcon from "../../icons/FilterIcon";
 
 const Navbar = () => {
+  const pathname = usePathname();
   return (
-    <div>navbar</div>
-  )
-}
+    <div className={styles.container}>
+      <div className={styles.title}>{pathname.split("/").pop()}</div>
+      <div className={styles.menu}>
+        <FilterIcon size="40" />
+        <div className={styles.search}>
+          <MdSearch />
+          <input type="text" placeholder="Search..." className={styles.input} />
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
