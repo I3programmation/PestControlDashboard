52f547c82c5b3a830d2fa15bbcb07bc5ac6e81f8
Hadi90fe <elhadiii90@gmail.com>
2023-12-13 14:39:06 -0500

    :fire: feat: enhanced Users page Added dummy users data , and created the table

--- app/dashboard/users/page.jsx
+++ app/dashboard/users/page.jsx
@@ -1,7 +1,123 @@
-const UsersPage = () => {
-  return (
-    <div>UsersPage</div>
-  )
-}
+import Search from "@/app/ui/dashboard/search/search";
+import styles from "@/app/ui/dashboard/users/users.module.css";
+import { Button, IconButton, Pagination } from "@mui/material";
+import Image from "next/image";
+import Link from "next/link";
+import { MdCreate, MdDelete, MdPreview } from "react-icons/md";
 
-export default UsersPage
+const UsersPage = () => {
+  const DATA = [
+    {
+      ID: 2134,
+      Name: "David Corby",
+      Email: "david.corby@gmail.com",
+      CreatedAt: "23/01/2023",
+      Role: "User",
+      Status: "active",
+    },
+    {
+      ID: 789,
+      Name: "Alice Johnson",
+      Email: "alice.johnson@example.com",
+      CreatedAt: "15/02/2023",
+      Role: "Admin",
+      Status: "inactive",
+    },
+    {
+      ID: 456,
+      Name: "Bob Smith",
+      Email: "bob.smith@example.com",
+      CreatedAt: "10/03/2023",
+      Role: "User",
+      Status: "active",
+    },
+    {
+      ID: 123,
+      Name: "Eva Rodriguez",
+      Email: "eva.rodriguez@example.com",
+      CreatedAt: "05/04/2023",
+      Role: "User",
+      Status: "inactive",
+    },
+    {
+      ID: 987,
+      Name: "Sophia Lee",
+      Email: "sophia.lee@example.com",
+      CreatedAt: "18/05/2023",
+      Role: "Admin",
+      Status: "active",
+    },
+  ];
+
+  return (
+    <div className={styles.container}>
+      {/* <div className={styles.top}>
+        <Search placeholder="Search for a user..." />
+        <Link href="/dashboard/users/add">
+          <button className={styles.addButton}>Add new</button>
+        </Link>
+      </div> */}
+      <table className={styles.table} rules="none">
+        <thead>
+          <tr>
+            <td>Name</td>
+            <td>Email</td>
+            <td>Created at</td>
+            <td>Role</td>
+            <td>Status</td>
+            <td></td>
+          </tr>
+        </thead>
+        <tbody>
+          {DATA.map((user) => (
+            <>
+            <tr className={styles.spacer}></tr>
+              <tr key={user.ID} className={styles.userRow}>
+                <td>
+                  <div className={styles.user}>
+                    <Image
+                      src="/noavatar.png"
+                      alt=""
+                      width={40}
+                      height={40}
+                      className={styles.userImage}
+                    />
+                    {user.Name}
+                  </div>
+                </td>
+                <td>{user.Email}</td>
+                <td>{user.CreatedAt}</td>
+                <td>{user.Role}</td>
+                <td>{user.Status}</td>
+                <td>
+                  <div className={styles.buttons}>
+                    <Link href="">
+                      <IconButton
+                        className={`${styles.button} ${styles.view}`}
+                        aria-label="View"
+                      >
+                        <MdCreate />
+                      </IconButton>
+                    </Link>
+                    <IconButton
+                      className={`${styles.button} ${styles.delete}`}
+                      aria-label="Delete"
+                    >
+                      <MdDelete color="#dc143c" />
+                    </IconButton>
+                  </div>
+                </td>
+              </tr>
+              <tr className={styles.spacer}></tr>
+            </>
+          ))}
+        </tbody>
+      </table>
+      <div className={styles.pagination}>
+        <Pagination count={4} variant="outlined" shape="rounded" />
+      </div>
+    </div>
+  );
+};
+
+export default UsersPage;
